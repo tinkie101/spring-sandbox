025bcab0401c5584815f4e6c05655a6f92251912
Albert <albert.volschenk@gmail.com>
2021-06-15 10:59:14 +0200

    Github ci/cd changes

--- env/postgres/db_scripts/adventureit.sql
+++ env/postgres/db_scripts/adventureit.sql
@@ -8,6 +8,8 @@ create table adventure.adventure
     description varchar(255),
     name        varchar(255)
 );
+alter table adventure.adventure
+    owner to adventure_user;
 
 INSERT INTO adventure.adventure (id, description, name) VALUES ('7dfd9998-c6b2-11eb-b8bc-0242ac130003', 'Test adventure 1', 'Adventure 1');
 INSERT INTO adventure.adventure (id, description, name) VALUES ('7dfd9a7e-c6b2-11eb-b8bc-0242ac130003', 'Test adventure 2', 'Adventure 2');
@@ -21,6 +23,8 @@ create table adventure.adventure_user
     adventure_id uuid,
     user_id      uuid
 );
+alter table adventure.adventure_user
+    owner to adventure_user;
 
 INSERT INTO adventure.adventure_user (id, adventure_id, user_id) VALUES ('c5f8f4b2-c6d6-11eb-b8bc-0242ac130003', '7dfd9998-c6b2-11eb-b8bc-0242ac130003', '7dfd9786-c6b2-11eb-b8bc-0242ac130003');
 INSERT INTO adventure.adventure_user (id, adventure_id, user_id) VALUES ('c5f8f7d2-c6d6-11eb-b8bc-0242ac130003', '7dfd9998-c6b2-11eb-b8bc-0242ac130003', 'ae4c2172-c6b3-11eb-b8bc-0242ac130003');
@@ -40,6 +44,8 @@ create table "user"."user"
     name    varchar(255),
     surname varchar(255)
 );
+alter table "user"."user"
+    owner to adventure_user;
 
 INSERT INTO "user"."user" (id, name, surname) VALUES ('7dfd9786-c6b2-11eb-b8bc-0242ac130003', 'Albert', 'Volschenk');
 INSERT INTO "user"."user" (id, name, surname) VALUES ('6d1e3ffa-c6b3-11eb-b8bc-0242ac130003', 'Latham', 'van der Walt');
