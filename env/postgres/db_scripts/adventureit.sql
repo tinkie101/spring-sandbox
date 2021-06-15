7e042b505ed6ea5aca066506c0817b4f5835aacf
Albert <albert.volschenk@gmail.com>
2021-06-15 10:27:40 +0200

    ci/cd changes

--- env/postgres/db_scripts/adventureit.sql
+++ env/postgres/db_scripts/adventureit.sql
@@ -1,31 +1,26 @@
 CREATE SCHEMA adventure;
 
-create table adventure
+create table adventure.adventure
 (
-    id          serial not null
+    id   uuid not null
         constraint adventure_pkey
             primary key,
     description varchar(255),
     name        varchar(255)
 );
 
-alter table adventure
-    owner to adventure_user;
 INSERT INTO adventure.adventure (id, description, name) VALUES ('7dfd9998-c6b2-11eb-b8bc-0242ac130003', 'Test adventure 1', 'Adventure 1');
 INSERT INTO adventure.adventure (id, description, name) VALUES ('7dfd9a7e-c6b2-11eb-b8bc-0242ac130003', 'Test adventure 2', 'Adventure 2');
 
 
-create table adventure_user
+create table adventure.adventure_user
 (
-    id           serial not null
+    id  uuid not null
         constraint adventure_user_pkey
             primary key,
     adventure_id uuid,
     user_id      uuid
 );
-
-alter table adventure_user
-    owner to adventure_user;
 
 INSERT INTO adventure.adventure_user (id, adventure_id, user_id) VALUES ('c5f8f4b2-c6d6-11eb-b8bc-0242ac130003', '7dfd9998-c6b2-11eb-b8bc-0242ac130003', '7dfd9786-c6b2-11eb-b8bc-0242ac130003');
 INSERT INTO adventure.adventure_user (id, adventure_id, user_id) VALUES ('c5f8f7d2-c6d6-11eb-b8bc-0242ac130003', '7dfd9998-c6b2-11eb-b8bc-0242ac130003', 'ae4c2172-c6b3-11eb-b8bc-0242ac130003');
@@ -37,17 +32,14 @@ INSERT INTO adventure.adventure_user (id, adventure_id, user_id) VALUES ('e471f8
 
 CREATE SCHEMA "user";
 
-create table "user"
+create table "user"."user"
 (
-    id      uuid not null
+    id  uuid not null
         constraint user_pkey
             primary key,
     name    varchar(255),
     surname varchar(255)
 );
-
-alter table "user"
-    owner to adventure_user;
 
 INSERT INTO "user"."user" (id, name, surname) VALUES ('7dfd9786-c6b2-11eb-b8bc-0242ac130003', 'Albert', 'Volschenk');
 INSERT INTO "user"."user" (id, name, surname) VALUES ('6d1e3ffa-c6b3-11eb-b8bc-0242ac130003', 'Latham', 'van der Walt');
